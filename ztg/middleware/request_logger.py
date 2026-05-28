b2b6fe94a8b401febc6f7b6739df32b77f76dbbb
paul-raja-dev <paulraja.creator@gmail.com>
2026-05-28 16:11:32 +0530

    feat - registration auto trigger for request logging

--- ztg/middleware/request_logger.py
+++ ztg/middleware/request_logger.py
@@ -1,3 +1,5 @@
+from ztg.models import RequestLog
+
 class RequestLoggerMiddleware:
 
     def __init__(self,get_response):
@@ -6,9 +8,21 @@ class RequestLoggerMiddleware:
     def __call__(self, request):
 
         print('Before Log.....')
-        print(f'[{request.method}]{request.path}')      
+        print(f'[{request.method}]{request.path}')
+
+        if request.path.startswith('/admin') or request.path.startswith('/static'):
+            return self.get_response(request) 
+
         response=self.get_response(request)
-        
+
+        RequestLog.objects.create(method=request.method
+                                  ,path=request.path,
+                                  status_code=response.status_code,
+                                  ip_address=request.META.get('REMOTE_ADDR'),
+                                  user_agent=request.META.get('HTTP_USER_AGENT')
+        )
+
+
         return response
 
         

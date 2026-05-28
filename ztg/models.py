fe0dfaea4e05c03796007540d7675ae75efcba58
paul-raja-dev <paulraja.creator@gmail.com>
2026-05-28 14:39:47 +0530

    create orm model for request logging

--- ztg/models.py
+++ ztg/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class RequestLog(models.Model):
+
+    method = models.CharField(max_length=10)
+    path = models.CharField(max_length=200)
+    status_code = models.IntegerField()
+    ip_address = models.CharField(max_length=100)
+    user_agent = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return f'{self.method} {self.path}'

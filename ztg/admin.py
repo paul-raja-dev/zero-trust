1a543fb8f0a34c8ecf98ce002ce2c45e2a044be0
paul-raja-dev <paulraja.creator@gmail.com>
2026-05-29 14:44:24 +0530

    add - BlockedIP ORM model base commit

--- ztg/admin.py
+++ ztg/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import RequestLog
+from .models import RequestLog,BlockedIP
 
 
 admin.site.register(RequestLog)
+admin.site.register(BlockedIP)
--- ztg/models.py
+++ ztg/models.py
@@ -15,3 +15,17 @@ class RequestLog(models.Model):
 
     def __str__(self):
         return f'{self.method} {self.path}'
+
+
+class BlockedIP(models.Model):
+
+    ip_address = models.CharField(max_length=50,unique=True)
+    reason = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    blocked_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-blocked_at']
+    
+    def __str__(self):  
+        return f'{self.ip_address} {self.blocked_at}'

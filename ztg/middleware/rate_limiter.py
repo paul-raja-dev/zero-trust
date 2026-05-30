f5273f95ccb1df3c23346ba2c883fb5b9f33ba97
paul-raja-dev <paulraja.creator@gmail.com>
2026-05-30 09:08:49 +0530

    counter logic to rate_limiter tuned version

--- ztg/middleware/rate_limiter.py
+++ ztg/middleware/rate_limiter.py
@@ -4,6 +4,8 @@ import time
 class RateLimiter:
 
     request_counts= {}
+    rate_limit = 5    # max request in given window_size
+    window_size = 10  # number of seconds to refresh the request count
 
     def __init__(self,get_response):
         self.get_response = get_response
@@ -16,7 +18,19 @@ class RateLimiter:
         lst_time_stamp = self.request_counts.get(ip_addr,[])
         lst_time_stamp.append(time.time())
 
-        self.request_counts[ip_addr] = lst_time_stamp
+        filtered_list =[]
+        current_time = time.time()
+        
+
+        for tim in lst_time_stamp:
+            if tim > current_time - self.window_size:
+                filtered_list.append(tim)
+
+
+        self.request_counts[ip_addr] = filtered_list
+
+        if len(filtered_list) > self.rate_limit:
+            return JsonResponse({'error' : 'Too many request'},status = 429)
 
         print(self.request_counts)
 
